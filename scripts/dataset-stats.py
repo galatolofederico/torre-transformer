c18d4f026792198b1252f727e4ea6842d17fe678
Federico Galatolo <galatolo.federico@gmail.com>
2022-03-30 17:16:36 +0200

    dataset stats

---
+++ scripts/dataset-stats.py
@@ -0,0 +1,32 @@
+import sys
+import hydra
+import numpy as np
+from tqdm import tqdm
+import json
+
+from src.dataset import get_dataset
+
+@hydra.main(config_path=None, config_name="config")
+def main(cfg):
+    ds = get_dataset(cfg, "train")
+    samples = dict()
+
+    for elem, sample in tqdm(zip(ds, range(0, cfg.args.samples)), total=cfg.args.samples):
+        for channel, channel_name in zip(elem.T, ds.channel_names):
+            if channel_name not in samples: samples[channel_name] = np.zeros((cfg.args.samples, cfg.dataset.window))
+            samples[channel_name][sample, :] = channel 
+
+    stats = dict()
+    for channel_name in ds.channel_names:
+        channel_samples = samples[channel_name].flatten()
+        channel_samples = channel_samples[~np.isnan(channel_samples)]
+        stats[channel_name] = dict(
+            mean=channel_samples.mean(),
+            std=channel_samples.std()
+        )
+    
+    print(json.dumps(stats, indent=4))
+
+if __name__  == "__main__":
+    sys.argv.append("args=dataset-stats")
+    main()
--- scripts/view-dataset.py
+++ scripts/view-dataset.py
@@ -1,3 +1,4 @@
+import sys
 import hydra
 import matplotlib.pyplot as plt
 
@@ -7,11 +8,9 @@ from src.dataset import get_dataset
 @hydra.main(config_path=None, config_name="config")
 def main(cfg):
     ds = get_dataset(cfg, "train")
-    subplots = (3, 3)
-    figsize = (10, 10)
 
     for elem in ds:
-        fig, axes = plt.subplots(*subplots, figsize=figsize)
+        fig, axes = plt.subplots(*cfg.args.subplots, figsize=cfg.args.figsize)
 
         for channel, channel_name, ax in zip(elem.T, ds.channel_names, axes.flatten()):
             ax.set_title(channel_name)
@@ -21,4 +20,5 @@ def main(cfg):
         plt.close(fig)
 
 if __name__  == "__main__":
+    sys.argv.append("args=view-dataset")
     main()
