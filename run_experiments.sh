a4a8b504cc7b21eb0839500a90d8cb78f718dac9
Federico Galatolo <galatolo.federico@gmail.com>
2022-05-11 13:37:59 +0200

    tag

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 WANDB=true
+TAG=testfull1
 
 mkdir -p ./models
 
-python train.py train.save_model=./models/VectorAutoRegressor/prepost.ckpt dataset=prepost train.wandb=$WANDB architecture=VectorAutoRegressor
-python train.py train.save_model=./models/VectorAutoRegressor/full.ckpt dataset=full train.wandb=$WANDB architecture=VectorAutoRegressor
-python train.py train.save_model=./models/VectorAutoRegressor/post.ckpt dataset=post train.wandb=$WANDB architecture=VectorAutoRegressor
+python train.py train.save_model=./models/VectorAutoRegressor/prepost.ckpt dataset=prepost train.wandb=$WANDB wandb.tag=$TAG architecture=VectorAutoRegressor
+python train.py train.save_model=./models/VectorAutoRegressor/full.ckpt dataset=full train.wandb=$WANDB wandb.tag=$TAG architecture=VectorAutoRegressor
+python train.py train.save_model=./models/VectorAutoRegressor/post.ckpt dataset=post train.wandb=$WANDB wandb.tag=$TAG architecture=VectorAutoRegressor
 
-python train.py train.save_model=./models/TransformerRegressor/prepost.ckpt dataset=prepost train.wandb=$WANDB architecture=TransformerRegressor
-python train.py train.save_model=./models/TransformerRegressor/full.ckpt dataset=full train.wandb=$WANDB architecture=TransformerRegressor
-python train.py train.save_model=./models/TransformerRegressor/post.ckpt dataset=post train.wandb=$WANDB architecture=TransformerRegressor
+python train.py train.save_model=./models/TransformerRegressor/prepost.ckpt dataset=prepost train.wandb=$WANDB wandb.tag=$TAG architecture=TransformerRegressor
+python train.py train.save_model=./models/TransformerRegressor/full.ckpt dataset=full train.wandb=$WANDB wandb.tag=$TAG architecture=TransformerRegressor
+python train.py train.save_model=./models/TransformerRegressor/post.ckpt dataset=post train.wandb=$WANDB wandb.tag=$TAG architecture=TransformerRegressor
